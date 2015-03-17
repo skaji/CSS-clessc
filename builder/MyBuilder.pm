2f6cd5426286c9ca92729e71c33a12eb1f52fd13
Shoichi Kaji <skaji@cpan.org>
2015-03-18 00:53:51 +0900

    update meta

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -4,11 +4,12 @@ use warnings;
 
 use parent 'Module::Build::XSUtil';
 use Config;
+use File::pushd 'pushd';
+use Devel::CheckBin qw(check_bin);
 
 sub new {
     my $class = shift;
     my $self = $class->SUPER::new(
-        generate_ppport_h    => "lib/CSS/ppport.h",
         needs_compiler_cpp   => 1,
         include_dirs         => "clessc/src",
         extra_linker_flags   => "-Lclessc/src -llessc",
@@ -16,14 +17,22 @@ sub new {
     );
 }
 
+sub create_build_script {
+    my $self = shift;
+    check_bin("sh");
+    check_bin("autoreconf");
+    $self->SUPER::create_build_script(@_);
+}
+
+sub run { !system @_ or die "Fail @_\n" }
+
 sub ACTION_code {
     my $self = shift;
     if (!-f "clessc/src/liblessc.a") {
-        !system join(' && ',
-            "cd clessc",
-            "./configure --without-libpng --without-libjpeg",
-            $Config{make},
-        ) or die "failed to make clessc";
+        my $guard = pushd "clessc";
+        run qw(sh autogen.sh);
+        run qw(sh configure --without-libglog --without-libpng --without-libjpeg);
+        run $Config{make};
     }
     $self->SUPER::ACTION_code(@_);
 }
@@ -31,14 +40,16 @@ sub ACTION_code {
 sub ACTION_clean {
     my $self = shift;
     if (-f "clessc/Makefile") {
-        system "cd clessc && $Config{make} clean &>/dev/null";
+        my $guard = pushd "clessc";
+        run $Config{make}, "clean";
     }
     $self->SUPER::ACTION_clean(@_);
 }
 sub ACTION_realclean {
     my $self = shift;
     if (-f "clessc/Makefile") {
-        system "cd clessc && $Config{make} distclean &>/dev/null";
+        my $guard = pushd "clessc";
+        run $Config{make}, "distclean";
     }
     $self->SUPER::ACTION_realclean(@_);
 }
--- cpanfile
+++ cpanfile
@@ -3,6 +3,11 @@ requires 'perl', '5.008001';
 on 'test' => sub {
     requires 'Test::More', '0.98';
     requires 'Test::LeakTrace';
-    requires 'Module::Build::XSUtil';
 };
 
+on configure => sub {
+    requires 'Module::Build::XSUtil';
+    requires 'Devel::CheckBin';
+    requires 'File::pushd';
+    requires 'parent';
+};
